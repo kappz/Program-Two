951958b51f0cfa9f996a594c93440fcdabee847c
kappz <addcolor117@gmail.com>
2017-06-20 12:48:45 -0400

    f

--- Program Two/Header.h
+++ Program Two/Header.h
@@ -1,4 +1,5 @@
 #pragma once
+#define DNAMANIP
 /*
 Author: Peter O'Donohue
 Creation Date: 06/12/17
@@ -13,6 +14,11 @@ struct Node
 {
 	char acid;
 	Node *next;
+	Node(char character = ' ', Node *ptr = nullptr)
+	{
+		acid = character;
+		next = ptr;
+	}
 };
 
 class DNA
@@ -24,25 +30,23 @@ public:
 	bool operator==(const DNA& rhs) const;
 	//returns true if rhs has the same acids in the same order to self
 	DNA operator=(const DNA& rhs);
-	// assigns rhs to self (with no memory leak)
-	DNA operator+(const DNA& rhs) const; // returns self + rhs
-	int find(const DNA& subStr) const;
+//	// assigns rhs to self (with no memory leak)
+//	DNA operator+(const DNA& rhs) const; // returns self + rhs
+//	int find(const DNA& subStr) const;
 	// returns the first position subStr in self exists or -1
-	DNA reverse() const; // returns a DNA strand reversed
-	friend ostream& operator<<(ostream & out, DNA rhs); // prints the strand from begin to end
+//	DNA reverse() const; // returns a DNA strand reversed
+//	friend ostream& operator<<(ostream & out, DNA rhs); // prints the strand from begin to end
 														// the first position is 0
 														// output gattaca.print(1,3) is att
-	DNA substr(int begin, int width) const;
+//	DNA substr(int begin, int width) const;
 	// returns the substr and from begin, returns the next width acids
-	int subSeq(const DNA rhs) const;
+//	int subSeq(const DNA rhs) const;
 	// returns location of first acid if self has a subsequence matching rhs, -1 otherwise
-	bool unitTestPlus();
-	bool unitTestSubstr();
-	bool unitTestFind();
+//	bool unitTestPlus();
+//	bool unitTestSubstr();
+//	bool unitTestFind();
 private:
 	Node *strand;
-	Node *head;
-	Node *copyHead;
 	int length;
 };
 
@@ -54,19 +58,24 @@ DNA::DNA(string dna)
 	POST: a link list has been created, with each node containing a single character of
 	the string that was passed through.
 	*/
-	strand = new Node;
-	head = strand;
-	strand->acid = dna.at(0);
-	strand->next = nullptr;
-	++length;
-	for (int i = 1; 1 < dna.size(); ++i)
+	Node *walker = nullptr;
+	strand = nullptr;
+	length = dna.length();
+	for (int i = 0; i < length; ++i)
 	{
-		strand->next = new Node;
-		strand->acid = dna.at(i);
-		strand->next = nullptr;
-		++length;
+		if (strand == nullptr)
+		{
+			strand = new Node(dna[i]);
+			walker = strand;
+		}
+		else
+		{
+			walker->next = new Node(dna[i]);
+			walker = walker->next;
+		}
 	}
 }
+
 
 DNA::DNA(const DNA& rhs)
 {
@@ -76,19 +85,29 @@ DNA::DNA(const DNA& rhs)
 	POST: a deep copy of the original argument has been created and 
 	temporarly stored in memory
 	*/
-	Node *oldWalker = head;
-	Node *copyStrand = new Node;
-	copyHead = copyStrand;
-	copyStrand->acid = oldWalker->acid;
-	copyStrand->next = nullptr;
-	while (oldWalker->next != nullptr)
+	
+	
+	Node *oldWalker = rhs.strand;
+	Node *newWalker = nullptr;
+	this->strand = nullptr;
+	while (oldWalker != nullptr)
 	{
+		if (this->strand == nullptr)
+		{
+			this->strand = new Node(oldWalker->acid);
+			newWalker = this->strand;
+		}
+		else
+		{
+			newWalker->next = new Node(oldWalker->acid);
+			newWalker = newWalker->next;
+		}
 		oldWalker = oldWalker->next;
-		copyStrand->next = new Node;
-		copyStrand->acid = oldWalker->acid;
-		copyStrand->next = nullptr;
-	}	
+	}
+	
+	cout << "Copy constructor called." << endl;
 }
+
 
 DNA::~DNA()
 {
@@ -96,9 +115,72 @@ DNA::~DNA()
 	PRE: an operation requiring a delete constructor has been performed
 	POST: an object has been deleted from memory
 	*/
+	
+	Node *deletePtr = this->strand;
+	while (deletePtr != nullptr)
+	{
+		this->strand = strand->next;
+		delete deletePtr;
+		deletePtr = this->strand;
+	}
+	
+	cout << "Destructor called." << endl;
 }
+
 
 bool DNA::operator==(const DNA& rhs) const
 {
-	
+	/*
+	PRE: two objects of type DNA have been created and their
+	strands initialized
+	POST: the two strands have been checked for equality
+	the function returns with true if the two strands are equal
+	and returns false if otherwise.
+	*/
+
+	Node *rhsWalker = rhs.strand;
+	Node *lhsWalker = this->strand;
+	if (length != rhs.length)
+	{
+		cout << "Not equal, strand lengths differ." << endl;
+		return false;
+	}
+	else
+		while (lhsWalker->next != nullptr)
+		{
+			if ( lhsWalker->acid == rhsWalker->acid)
+			{
+				lhsWalker = lhsWalker->next;
+				rhsWalker = rhsWalker->next;
+			}
+			else
+				return false;
+		}
+	return true;
 }
+
+
+
+DNA DNA::operator=(const DNA& rhs)
+
+{
+	Node *rhsWalker = rhs.strand;
+	Node *lhsWalker = this->strand;
+	Node *newWalker = nullptr;
+	Node *deletePtr = lhsWalker;
+	this->strand = new Node(rhsWalker->acid);
+	newWalker = this->strand->next;
+	while (rhsWalker != nullptr)
+	{
+		
+		newWalker = new Node(rhsWalker->acid);
+		rhsWalker = rhsWalker->next;
+		newWalker = newWalker->next;
+		lhsWalker = lhsWalker->next;
+		delete deletePtr;
+		deletePtr = lhsWalker;
+		
+	}
+	return *this;
+}
+
--- Program Two/Main.cpp
+++ Program Two/Main.cpp
@@ -1,9 +1,20 @@
-#include <iostream>
+#include "Header.h"
+#include <iostream>;
+#include <string>
+
 using namespace std;
 
 int main()
 {
 
+	DNA a("acgt");
+	DNA b("abgt");
+	if (a == b)
+	{
+		cout << "Operator == function called" << endl;
+	}
+	a = b;
+	
 	system("pause");
-	return 1;
+	return 0;
 }
